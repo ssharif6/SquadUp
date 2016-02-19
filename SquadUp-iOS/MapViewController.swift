49871dbe8617307c24d659ca46707316c739d1bb
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2016-02-19 01:50:51 -0800

    Added MapsVC

--- SquadUp-iOS/MapViewController.swift
+++ SquadUp-iOS/MapViewController.swift
@@ -9,18 +9,35 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate {
+class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegate, CLLocationManagerDelegate {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var useCurrentLocationButton: FacebookLoginButton!
     @IBOutlet weak var locationMapView: MKMapView!
-    
-    var locationManager: CLLocationManager()
-    
+    var locationManager: CLLocationManager?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        locationManager.delegate = self
+        locationManager = CLLocationManager()
+        locationManager!.delegate = self
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.startUpdatingLocation()
+        locationMapView.showsUserLocation = true
         
+       
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var location = locations.last
+        let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
+        let region = MKCoordinateRegionMake(center, MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
+        self.locationMapView.setRegion(region, animated: true)
+        locationManager?.stopUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager, didFailWithError error: NSError) {
+        print(error.localizedDescription)
     }
     
     
