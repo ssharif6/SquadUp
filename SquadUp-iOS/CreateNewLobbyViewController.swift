9b54d0fe1e822b47105dd72e069f7b4d88635ea3
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2016-02-19 21:44:39 -0800

    Added MapView Features

--- SquadUp-iOS/CreateNewLobbyViewController.swift
+++ SquadUp-iOS/CreateNewLobbyViewController.swift
@@ -9,27 +9,24 @@
 import UIKit
 
 class CreateNewLobbyViewController: UIViewController {
-
+    @IBOutlet weak var lobbyNameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var numPlayersTextField: UITextField!
+    @IBOutlet weak var locationAddressTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func createLobbyPressed (sender: AnyObject) {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func findLocationButtonPressed(sender: AnyObject) {
+        
     }
-    */
-
+    
+    
 }
--- SquadUp-iOS/MapViewController.swift
+++ SquadUp-iOS/MapViewController.swift
@@ -24,12 +24,11 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
         locationManager?.requestWhenInUseAuthorization()
         locationManager?.startUpdatingLocation()
         locationMapView.showsUserLocation = true
-        
        
     }
     
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        var location = locations.last
+        let location = locations.last
         let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
         let region = MKCoordinateRegionMake(center, MKCoordinateSpan(latitudeDelta: 0.03, longitudeDelta: 0.03))
         self.locationMapView.setRegion(region, animated: true)
@@ -40,6 +39,69 @@ class MapViewController: UIViewController, UISearchBarDelegate, MKMapViewDelegat
         print(error.localizedDescription)
     }
     
+    @IBAction func useCurrentLocationPressed(sender: AnyObject) {
+        let location = CLLocation(latitude: locationMapView.userLocation.coordinate.latitude, longitude: locationMapView.userLocation.coordinate.longitude)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = self.locationMapView.userLocation.coordinate
+        CLGeocoder().reverseGeocodeLocation(location) { (placemarks: [CLPlacemark]?, error: NSError?) -> Void in
+            var title = ""
+            var subtitle = ""
+            if error == nil {
+                if let placemark = placemarks?[0] {
+                    var subThoroughfare: String = ""
+                    var thoroughfare: String = ""
+                    var locality: String = ""
+                    var postalCode: String = ""
+                    var adminArea: String = ""
+                    var country: String = ""
+                    
+                    if placemark.subThoroughfare != nil {
+                        subThoroughfare = placemark.subThoroughfare!
+                    }
+                    if placemark.thoroughfare != nil {
+                        thoroughfare = placemark.thoroughfare!
+                    }
+                    if placemark.locality != nil {
+                        locality = placemark.locality!
+                    }
+                    if placemark.postalCode != nil {
+                        postalCode = placemark.postalCode!
+                    }
+                    if placemark.administrativeArea != nil {
+                        adminArea = placemark.administrativeArea!
+                    }
+                    if placemark.country != nil {
+                        country = placemark.country!
+                    }
+                    title = " \(subThoroughfare) \(thoroughfare) \(locality), \(adminArea)"
+                    subtitle = " \(subThoroughfare) \(thoroughfare)";
+                }
+                
+                annotation.title = title
+                annotation.subtitle = "Current Location"
+                self.locationMapView.addAnnotation(annotation)
+                self.useCurrentLocationButton.enabled = false
+            }
+
+        }
+        
+    }
     
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseId = "pin"
+        var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId)
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            annotationView?.canShowCallout = true
+            
+        } else {
+            annotationView?.annotation = annotation
+        }
+        return annotationView
+    }
+    
+    func geoCode() {
+        
+    }
 
 }
