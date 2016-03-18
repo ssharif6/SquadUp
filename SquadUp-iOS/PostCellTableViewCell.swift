100df4527ee137f3ae631ae537e05ed1aa07eecd
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2016-03-18 14:38:38 -0700

    Hotfixes

--- SquadUp-iOS/PostCellTableViewCell.swift
+++ SquadUp-iOS/PostCellTableViewCell.swift
@@ -22,6 +22,8 @@ class PostCellTableViewCell: UITableViewCell {
     var post: Post!
     var request = Request?()
     var likeRef: Firebase!
+    var cache = NSCache()
+
 
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -40,35 +42,49 @@ class PostCellTableViewCell: UITableViewCell {
 
 
     func configureCell(post: Post, image: UIImage?) {
-        self.post = post
-        likeRef = DataService.ds.REF_USER_CURRENT.childByAppendingPath("likes").childByAppendingPath(post.postKey)
-        self.nameLabel.text = post.name
-        self.activityLabel.text = post.activity
-        self.numLikes.text = "\(post.numLikes)"
-        
-        if post.imageUrl != nil {
-            if image != nil {
-                self.profileImage.image = image
-            } else {
-                request = Alamofire.request(.GET, post.imageUrl!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, error in
-                    if error == nil {
-                        let image = UIImage(data: data!)!
-                        self.profileImage.image = image
-                        FeedVCViewController.imageCache.setObject(image, forKey: self.post.imageUrl!)
-                    }
-                })
+        if (cache.objectForKey("cachedCell") != nil) {
+            // load from Cached Version
+        } else {
+            // Load and send to Cache
+            self.post = post
+            likeRef = DataService.ds.REF_USER_CURRENT.childByAppendingPath("likes").childByAppendingPath(post.postKey)
+            self.nameLabel.text = post.name
+            self.activityLabel.text = post.activity
+            self.numLikes.text = "\(post.numLikes)"
+            
+            if post.imageUrl != nil {
+                if image != nil {
+                    self.profileImage.image = image
+                } else {
+                    request = Alamofire.request(.GET, post.imageUrl!).validate(contentType: ["image/*"]).response(completionHandler: { request, response, data, error in
+                        if error == nil {
+                            let image = UIImage(data: data!)!
+                            self.profileImage.image = image
+                            FeedVCViewController.imageCache.setObject(image, forKey: self.post.imageUrl!)
+                        }
+                    })
+                }
             }
+            // Reference for likes
+            
+            likeRef.observeSingleEventOfType(.Value, withBlock:{snapshot in
+                if let doesNotExist = snapshot.value as? NSNull {
+                    // Post is not liked
+                    self.likeImage.image = UIImage(named: "heart-empty")
+                } else {
+                    self.likeImage.image = UIImage(named: "heart-full")
+                }
+            })
+            
+            cache.setObject("Cached", forKey: "cachedCell")
+            cache.setObject(post, forKey: "cachedPost")
+            cache.setObject(post.name, forKey: "cachedPostName")
+            cache.setObject(post.activity, forKey: "cachedActivity")
+            cache.setObject(post.numLikes, forKey: "cachedNumLikes")
+            cache.setObject(post.imageUrl!, forKey: "cachedImageUrl")
+            
         }
-       // Reference for likes
         
-        likeRef.observeSingleEventOfType(.Value, withBlock:{snapshot in
-            if let doesNotExist = snapshot.value as? NSNull {
-                // Post is not liked
-                self.likeImage.image = UIImage(named: "heart-empty")
-            } else {
-                self.likeImage.image = UIImage(named: "heart-full")
-            }
-        })
         
     }
     
