fcb3a17af90f3f0e94c8c603ea8ce157c6a82608
Shaheen Sharifian <shaheen.sharifian@gmail.com>
2016-03-18 00:40:40 -0700

    Added Messages VC

---
+++ SquadUp-iOS/ChatViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ChatViewController.swift
+//  SquadUp-iOS
+//
+//  Created by Shaheen Sharifian on 3/17/16.
+//  Copyright © 2016 Shaheen Sharifian. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
+    
+    var messagesArray:[String] = [String]()
+    
+    @IBOutlet weak var dockViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messagesTextField: UITextField!
+    @IBOutlet weak var messagesTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.messagesTextField.delegate = self
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "tableViewTapped")
+        self.messagesTableView.addGestureRecognizer(tapGesture)
+    }
+
+    @IBAction func sendButtonTapped(sender: UIButton) {
+        // perform animation to grow the dock view
+        self.messagesTextField.endEditing(true)
+        
+    }
+    
+    func tableViewTapped() {
+        self.messagesTextField.endEditing(true)
+    }
+    
+    func downloadMessages() {
+        
+    }
+    
+    // MARK: Textfield Delegate Methods
+    func textFieldDidBeginEditing(textField: UITextField) {
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(0.3, animations: {
+            self.dockViewHeightConstraint.constant = 300
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        self.view.layoutIfNeeded()
+        UIView.animateWithDuration(0.4, animations: {
+            self.dockViewHeightConstraint.constant = 60
+            self.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+    
+    // MARK: TableView Delegate Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messagesArray.count
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        // Do something
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("MessageCell")
+        return cell!
+    }
+
+}
